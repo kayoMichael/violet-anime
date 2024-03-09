7c4df5c2384a4cb8d19c233c88c4aa9fa0100a92
kayoMichael <michaelxkayo@hotmail.com>
2024-03-09 15:53:42 +0900

    add prettier and import sort

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,80 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+        project: ['./tsconfig.json'],
+      },
     },
-    extends: [
-        "standard-with-typescript",
-        "plugin:react/recommended",
-        "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'simple-import-sort'],
+  ignorePatterns: [
+    'node_modules',
+    'public',
+    'next-env.d.ts',
+    '.next',
+    'lib/seedTriggers.ts',
+    'utils/**/*',
+  ],
+  rules: {
+    'react/jsx-sort-props': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: 'import',
+        next: '*',
+      },
+      {
+        blankLine: 'any',
+        prev: 'import',
+        next: 'import',
+      },
     ],
-    overrides: [
-        {
-            env: {
-                node: true,
-            },
-            files: [".eslintrc.{js,cjs}"],
-            parserOptions: {
-                sourceType: "script",
-            },
-        },
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'all',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
     ],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
+    '@typescript-eslint/no-useless-template-literals': 'error',
+    '@typescript-eslint/no-useless-empty-export': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    plugins: ["react"],
-    rules: {},
-    settings: {
-        react: {
-            version: "latest",
-        },
-    },
+  },
 };
